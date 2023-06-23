0b02fba37709eb253561cb2586a5db18148d5a77
Federico La Morgia <federico.lamorgia@gmail.com>
2023-06-23 20:12:47 +0200

    Modificata la logica di scaricamento delle immagini cloudinit minimizzando le operazioni da effettuare

--- scripts/bash/simple-script/VM/cloud-init/scaricamentoImmaginiCloudInit.sh
+++ scripts/bash/simple-script/VM/cloud-init/scaricamentoImmaginiCloudInit.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# TODO LIST
-# -) creare un elenco di files in cui saranno presenti i links da cui scaricare le immagini cloudinit dei vari sistemi operativi scelti alle varie versioni
-
-offset=9000
-
-lastIdUsed=$(qm list | grep -vi 'vmid' | awk '{print $1}' | sort | tail -1)
-
-nextId=$(( lastIdUsed + 1 ))
+# Lista variabili input
+# $1 : url del file cloudinit da scaricare
+# $2 : path dove scaricare il file di cloudinit
 
 defaultPath=/var/lib/vz/template/cloudinit
 
@@ -18,29 +13,29 @@ else
 	echo "La directory esiste !"
 fi
 
-readarray -d '' -t targets < <(grep --null -ri "http" ./files | awk -F ".cfg" '{print $2}')
+url=$1
+
+destinazione=$2
 
 #sizeList=${#listDownload[@]}
 
 echo "dimensione array : ${#targets[@]}"
 
-for str in ${targets[@]}; do
-	my_arr=($(echo $str | tr "/" "\n"))
-	nomeFile=${my_arr[-1]}
-	echo "$nomeFile"
-	if [[ ! -f $defaultPath/$nomeFile ]]; then
-		echo "il file non esiste !"
-		ls -la $defaultPath/$nomeFile
-		wget -P $defaultPath $str
-		/usr/bin/ls $defaultPath/$nomeFile
-		retVal=$?
-		echo "#############################"
-		echo "stato di uscita del wget : $retVal"
-		echo "#############################"
-		echo "Scaricato il file $nomeFile in $defaultPath"
-		ls -la $defaultPath/$nomeFile
-	else
-		echo "Il file $defaultPath/$nomeFile esiste"
-	fi
-done
-
+my_arr=($(echo $url | tr "/" "\n"))
+echo "my_arr : $my_arr"
+nomeFile=${my_arr[-1]}
+echo "$nomeFile"
+#if [[ ! -f $defaultPath/$nomeFile ]]; then
+#	echo "il file non esiste !"
+#	ls -la $defaultPath/$nomeFile
+#	wget -P $defaultPath $str
+#	/usr/bin/ls $defaultPath/$nomeFile
+#	retVal=$?
+#	echo "#############################"
+#	echo "stato di uscita del wget : $retVal"
+#	echo "#############################"
+#	echo "Scaricato il file $nomeFile in $defaultPath"
+#	ls -la $defaultPath/$nomeFile
+#else
+#	echo "Il file $defaultPath/$nomeFile esiste"
+#fi
