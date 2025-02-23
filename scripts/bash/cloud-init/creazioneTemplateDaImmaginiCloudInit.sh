f4196e33ca470b2a97ee21ea51aea9dfb7d635fa
root <root@pve-zia.local>
2025-02-23 10:48:38 +0100

    Aggiunto script creazione template da immagini cloud init

--- scripts/bash/cloud-init/creazioneTemplateDaImmaginiCloudInit.sh
+++ scripts/bash/cloud-init/creazioneTemplateDaImmaginiCloudInit.sh
@@ -5,7 +5,7 @@
 # $2 : pool storage
 
 # Utilizzo dello script
-# ./creazioneTemplateDaImmaginiCloudInit.sh <path immagine cloudinit>
+# ./creazioneTemplateDaImmaginiCloudInit.sh <path immagine cloudinit> <pool statorage>
 
 if [ ! $1 == "" ] ; then
 	echo "Percorso della immagine specificato"
@@ -43,27 +43,27 @@ pvesh get nodes --output-format json | jq '.| length'
 pvesh get nodes --output-format json | jq -r '.[0].node'
 
 # Dall'id offset in poi ci sono i template delle VM
-#offset=9000
-#
-#lastID=$(qm list | grep -vi 'vmid' | awk '{print $1}' | sort -n | tail -1)
-#
-#if [[ $lastID -le $offset ]] ; then
-#	nextID=$(( offset + 1 ))
-#elif [[ $lastID -gt $offset ]] ; then
-#	nextID=$(( lastID + 1 ))
-#fi
-#
-#qm create $nextID --memory 1024 --cores 1 --net0 virtio,bridge=vmbr0 ;
-#
-#qm importdisk $nextID $immagine local-lvm ;
-#
-#qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0 ;
-#
-#qm set 9000 --ide2 local-lvm:cloudinit ;
-#
-#qm set 9000 --boot c --bootdisk scsi0 ;
-#
-#qm set 9000 --serial0 socket --vga serial0 ; # è necessario altrimenti non funziona l'accesso alla vm visto che è pensata per girare sotto openstack
-#
-#qm template 9000 ;
+offset=9000
+
+lastID=$(qm list | grep -vi 'vmid' | awk '{print $1}' | sort -n | tail -1)
+
+if [[ $lastID -le $offset ]] ; then
+	nextID=$(( offset + 1 ))
+elif [[ $lastID -gt $offset ]] ; then
+	nextID=$(( lastID + 1 ))
+fi
+
+qm create $nextID --memory 1024 --cores 1 --net0 virtio,bridge=vmbr0 ;
+
+qm importdisk $nextID $immagine local-lvm ;
+
+qm set $nextID --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$nextID-disk-0 ;
+
+qm set $nextID --ide2 local-lvm:cloudinit ;
+
+qm set $nextID --boot c --bootdisk scsi0 ;
+
+qm set $nextID --serial0 socket --vga serial0 ; # è necessario altrimenti non funziona l'accesso alla vm visto che è pensata per girare sotto openstack
+
+qm template $nextID ;
 
