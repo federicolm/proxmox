b114955540dcb5cf12066b267bf33ac4feb8457b
root <root@pve-zia.local>
2025-04-17 20:21:26 +0200

    Inizio creazione script checkPresenzaFile

---
+++ scripts/bash/cloud-init/checkPresenzaFile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Lo scopo di questo script è verificare la presenza, in un determinato path
+# all'interno di un nodo proxmox oppure di un pool di storage condiviso in un cluster
+# oppure in uno storage remoto, di un determinato file passato come URL in ingresso e relativo hash md5/sha
+#
+#
