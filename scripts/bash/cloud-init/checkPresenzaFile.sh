1521bc22f2e0df6536e6e23f9cd0fc882d05796a
root <root@pve-zia.local>
2025-04-17 20:28:49 +0200

    da completare

--- scripts/bash/cloud-init/checkPresenzaFile.sh
+++ scripts/bash/cloud-init/checkPresenzaFile.sh
@@ -4,4 +4,22 @@
 # all'interno di un nodo proxmox oppure di un pool di storage condiviso in un cluster
 # oppure in uno storage remoto, di un determinato file passato come URL in ingresso e relativo hash md5/sha
 #
+# Parametri di input : 
+# $1 : pool di storage
+# $2 : hash del file md5/sha
+# $3 : URL dell'immagine qcow2/iso da verificare o da scaricare
 #
+# Utilizzo dello script : ./checkPresenzaFile.sh <pool di storage> <hash del file> <url del file> 
+# 
+# Versione : 0.0.1
+# Data : 17/04/2025
+# Autore : Federico La Morgia <federico.lamorgia@gmail.com>
+#
+
+pool=$1
+hash=$2
+url=$3
+
+echo "Parametro 1 : $pool"
+echo "Parametro 2 : $hash"
+echo "Parametro 3 : $url"
